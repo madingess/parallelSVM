a9e7b2c468f12294dc3b68e709314bf74078707b
madingess <madingess@yahoo.com>
2020-05-02 14:53:27 -0400

    Added barebones files. Makefile should work. ParallelSVM.java is a clone of my HW2 Part1. test1.csv is empty.

---
+++ Makefile
@@ -0,0 +1,34 @@
+run: jar
+	hadoop fs -rm -f -r  /user/cloudera/parallelSVM/output
+	hadoop jar parralelSVM.jar org.myorg.parallelSVM /user/cloudera/parallelSVM/input /user/cloudera/parallelSVM/output
+
+compile: build/org/myorg/ParallelSVM.class
+
+jar: parallelSVM.jar
+
+part.jar: build/org/myorg/ParallelSVM.class
+	jar -cvf parallelSVM.jar -C build/ .
+
+build/org/myorg/ParallelSVM.class: ParallelSVM.java
+	mkdir -p build
+	javac -cp /usr/lib/hadoop/*:/usr/lib/hadoop-mapreduce/* ParallelSVM.java -d build -Xlint
+
+clean:
+	rm -rf build parallelSVM.jar 
+
+putTestData: mkdirs
+	hadoop fs -put inputs/test1.csv /user/cloudera/parallelSVM/input
+
+putFullData: mkdirs
+	hadoop fs -put inputs/FILENAME.csv /user/cloudera/parallelSVM/input
+
+mkdirs:
+	hadoop fs -rm -f -r /user/cloudera/parallelSVM/input
+	hadoop fs -rm -f -r /user/cloudera/parallelSVM/output
+	hadoop fs -mkdir -p /user/cloudera/parallelSVM/
+	hadoop fs -mkdir /user/cloudera/parallelSVM/input/
+	hadoop fs -mkdir /user/cloudera/parallelSVM/output/
+
+showResult:
+	hadoop fs -cat /user/cloudera/parallelSVM/output/*
+
---
+++ ParallelSVM.java
@@ -0,0 +1,143 @@
+package org.myorg;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.log4j.Logger;
+
+public class ParallelSVM extends Configured implements Tool {
+
+  private static final Logger LOG = Logger.getLogger(Part1.class);
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Part1(), args);
+    System.exit(res);
+  }
+
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(getConf(), "part1");
+    job.setJarByClass(this.getClass());
+    // Use TextInputFormat, the default unless job.setInputFormatClass is used
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    job.setMapOutputKeyClass(IntWritable.class);
+    job.setMapOutputValueClass(DoubleWritable.class);
+
+    // Wait for completion; return completion value at the end of the method
+    Boolean ret = job.waitForCompletion(true);
+    return ret ? 0 : 1;
+  }
+
+
+  public static class Map extends Mapper<LongWritable, Text, IntWritable, DoubleWritable> {
+
+    /* 
+     * THIS IS CURRENTLY JUST A COPY OF MY HW2 Part1.java file 
+     *
+     * TODO: 
+     *       Delete current Mapper logic
+     *       Implemente parallelSVM Mapper logic
+     *       May need to change MapOutput Key/Value Class in main
+     */
+
+    private final static IntWritable male = new IntWritable(1);
+    private final static IntWritable female = new IntWritable(2);
+    private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*,\\s*");
+
+    public void map(LongWritable offset, Text lineText, Context context) 
+        throws IOException, InterruptedException {
+      String line = lineText.toString();
+
+      // Parse the line as:
+      //    pid  gender  race  height  weight  asthma  hypertension
+      // Split should give array of seven string
+      String[] linesplit = WORD_BOUNDARY.split(line);
+
+      LOG.info("HERE BE SPLIT LINE");
+      LOG.info(String.valueOf(linesplit.length));
+      LOG.info(linesplit[0]);
+      LOG.info(linesplit[1]);
+      LOG.info(linesplit[2]);
+      LOG.info(linesplit[3]);
+      
+      if (linesplit.length != 7)
+        throw new IOException("Line Parsing Failed.");
+      
+      // Check that we are not operating on the header line of the csv
+      //   and that the gender is not null
+      //   and that the height is not null
+      if(linesplit[0].compareTo("pid") != 0 && 
+         linesplit[1].compareTo("NULL") != 0) {
+   
+        String gender = linesplit[1];
+        DoubleWritable height;
+        if (linesplit[3].compareTo("NULL") == 0)
+          height = new DoubleWritable((Double) 0.0);
+        else
+          height = new DoubleWritable(Double.parseDouble(linesplit[3]));
+
+        if (gender.compareTo("1") == 0)    // if male
+          context.write(male, height);
+        else                               // else female
+          context.write(female, height);
+      }
+    }
+  }
+
+
+  public static class Reduce extends Reducer<IntWritable, DoubleWritable, Text, Text> {
+
+    /* 
+     * THIS IS CURRENTLY JUST A COPY OF MY HW2 Part1.java file 
+     *
+     * TODO: 
+     *       Delete current Reducer logic
+     *       Implemente parallelSVM Reducer logic
+     *       May need to change Output Key/Value Class in main
+     */
+
+    private final static IntWritable male = new IntWritable(1);
+    private final static IntWritable female = new IntWritable(2);
+    private final static Text maleText = new Text("male");
+    private final static Text femaleText = new Text("female");
+
+    @Override
+    public void reduce(IntWritable gender, Iterable<DoubleWritable> heights, Context context)
+        throws IOException, InterruptedException {
+
+      int genderCount = 0;
+      double maxHeight = 0.0;
+      for (DoubleWritable height : heights) {
+        genderCount += 1;
+        if (height.get() > maxHeight) {
+          maxHeight = height.get();
+        }
+      }
+
+      Text outputGenderHeight = new Text(String.valueOf(genderCount) + " " + String.valueOf(maxHeight));
+
+      // Write to context
+      if (gender.equals(male)) 
+        context.write(maleText, outputGenderHeight);
+      else 
+        context.write(femaleText, outputGenderHeight);
+    }
+  }
+}
